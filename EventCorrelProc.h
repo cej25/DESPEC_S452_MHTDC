6cc7fb66853c7b810d4460975cc1d000a0f506b7
cej25 <44805579+cej25@users.noreply.github.com>
2022-05-05 15:46:49 +0200

    define missing histograms

--- EventCorrelProc.h
+++ EventCorrelProc.h
@@ -335,29 +335,34 @@ class EventCorrelProc : public TGo4EventProcessor {
 
       TH1 *hA_FRSWR_GeWR;
       TH1 *hA_FRS_GeE;
-      TH2 *hA_FRS_ZAoQ_GeEvsT_all;
-      TH1 *hA_FRS_ZAoQ_GeE[MAX_FRS_GATE];
-      TH1 *hA_FRS_Z1Z2_X2AoQ_GeE[MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X2AoQ_GeEvsT[MAX_FRS_GATE];
-      TH1 *hA_FRS_Z1Z2_X4AoQ_GeE[MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X4AoQ_GeEvsT[MAX_FRS_GATE];
+      TH2 *hA_FRS_ZAoQ_GeEvsT_all; // done
+      TH1 *hA_FRS_ZAoQ_GeE[MAX_FRS_GATE]; // done
+      TH1 *hA_FRS_Z1Z2_X2AoQ_GeE[MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X2AoQ_GeEvsT[MAX_FRS_GATE]; // done
+      TH1 *hA_FRS_Z1Z2_X4AoQ_GeE[MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X4AoQ_GeEvsT[MAX_FRS_GATE]; // done
      // TH2 *hA_FRS_GeEvsT;
-      TH2 *hA_FRS_ZAoQ_GeEvsT[MAX_FRS_GATE];
-      TH2 *hA_FRS_ZAoQ_GeE1_GeE2[MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X2AoQ_GeE1_GeE2[MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X4AoQ_GeE1_GeE2[MAX_FRS_GATE];
+      TH2 *hA_FRS_ZAoQ_GeEvsT[MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_ZAoQ_GeE1_GeE2[MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X2AoQ_GeE1_GeE2[MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X4AoQ_GeE1_GeE2[MAX_FRS_GATE]; // done
 
-      TH2 *hA_FRS_ZAoQ_GeEvsT_all_mhtdc[10];
-      TH1 *hA_FRS_ZAoQ_GeE_mhtdc[10][MAX_FRS_GATE];
-      TH1 *hA_FRS_Z1Z2_X2AoQ_GeE_mhtdc[10][MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X2AoQ_GeEvsT_mhtdc[10][MAX_FRS_GATE];
-      TH1 *hA_FRS_Z1Z2_X4AoQ_GeE_mhtdc[10][MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X4AoQ_GeEvsT_mhtdc[10][MAX_FRS_GATE];
+      // cej commments
+      TH1 *hA_FRS_Z1Z2_GeE_mhtdc[10][MAX_FRS_GATE];
+      TH2 *hA_FRS_Z1Z2_GeEvsT_mhtdc[10][MAX_FRS_GATE];
+      THS *hA_FRS_Z1Z2_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE];
+
+      TH2 *hA_FRS_ZAoQ_GeEvsT_all_mhtdc[10]; // done
+      TH1 *hA_FRS_ZAoQ_GeE_mhtdc[10][MAX_FRS_GATE]; // done
+      TH1 *hA_FRS_Z1Z2_X2AoQ_GeE_mhtdc[10][MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X2AoQ_GeEvsT_mhtdc[10][MAX_FRS_GATE]; // done
+      TH1 *hA_FRS_Z1Z2_X4AoQ_GeE_mhtdc[10][MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X4AoQ_GeEvsT_mhtdc[10][MAX_FRS_GATE]; // done
      // TH2 *hA_FRS_GeEvsT;
-      TH2 *hA_FRS_ZAoQ_GeEvs_mhtdc[10][MAX_FRS_GATE];
-      TH2 *hA_FRS_ZAoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X2AoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE];
-      TH2 *hA_FRS_Z1Z2_X4AoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE];
+      TH2 *hA_FRS_ZAoQ_GeEvsT_mhtdc[10][MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_ZAoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X2AoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE]; // done
+      TH2 *hA_FRS_Z1Z2_X4AoQ_GeE1_GeE2_mhtdc[10][MAX_FRS_GATE]; // done
 
       TH1 *hA_FRS_ZAoQ_GeE_LongIso;
       TH2 *hA_FRS_GeEvsT_LongIsoGated;
