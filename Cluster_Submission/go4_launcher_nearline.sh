41cff416f44ba61f6bb8a0acf1fae42f59c29b96
cej25 <44805579+cej25@users.noreply.github.com>
2022-05-05 12:44:44 +0200

    cluster submission files

--- Cluster_Submission/go4_launcher_nearline.sh
+++ Cluster_Submission/go4_launcher_nearline.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 ##Setup environment
-source /cvmfs/csee.gsi.de/bin/go4login
-export ROOT_INCLUDE_PATH=/lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421
+source /cvmfs/eel.gsi.de/centos7-x86_64/bin/go4login
+export ROOT_INCLUDE_PATH=/lustre/gamma/cjones/DESPEC_S452_MHTDC
 echo "DESPEC Kronos Started at `date`"
 
 ##Set data location
 #dpath=~/lustre/gamma/d004/ts/aida/
 
-##Read in list of files to run. Format names seperated by space,tab,newline
-#LISTFILE="/lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421/Cluster_Submission/file_list_f187188_testfatgain.txt"
-
-#LISTFILE="/lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421/Cluster_Submission/file_list_full_f74_f75.txt"
-
-LISTFILE="/lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421/Cluster_Submission/file_list_full_188Ta.txt"
+LISTFILE="/lustre/gamma/cjones/DESPEC_S452_MHTDC/Cluster_Submission/file_list_full_188Ta.txt"
 
 ##Count number of files
 NFILES=$(cat ${LISTFILE} | wc -l)
@@ -30,7 +25,4 @@ echo "Array is $SLURM_ARRAY_TASK_ID"
 part=(  "${array[@]:$SLURM_ARRAY_TASK_ID:5}" ) # :5 number of files to put together -> Has to be the same in the 2 .sh scripts
 
 echo "Running Go4!"
-go4analysis -file ${part[*]} -enable-asf 1800 -asf /lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421/Cluster_Submission/Nearline_Histograms/ANALY18_allTa_$SLURM_ARRAY_TASK_ID.root
-
-#go4analysis -file ${part[*]} -enable-asf 1800 -asf /lustre/gamma/amistry/DESPEC_S452_NEARLINE_200421/Cluster_Submission/Nearline_Histograms/ANALYf74f75_$SLURM_ARRAY_TASK_ID.root
-
+go4analysis -file ${part[*]} -enable-asf 1800 -asf /lustre/gamma/cjones/DESPEC_S452_MHTDC/Cluster_Submission/Nearline_Histograms/ANALY18_allTa_$SLURM_ARRAY_TASK_ID.root
